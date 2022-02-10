25f3a8289a3ef8ffa6ae8b27fb7fa07c0f3a1002
Michael Kaufman <michaelkauf@gmail.com>
2022-02-09 21:53:28 -0800

    day4 pt1

---
+++ lib/day4.ex
@@ -0,0 +1,37 @@
+defmodule AdventOfCode.Day4 do
+  @moduledoc """
+  Documentation for `AdventOfCode.Day4`.
+  """
+
+  def input do
+    "priv/day4/input.txt"
+    |> File.read!()
+    |> String.split("\n\n", trim: true)
+    |> Enum.map(&String.replace(&1, "\n", " "))
+    |> Enum.map(&parse/1)
+  end
+
+  def parse(line) do
+    line
+    |> String.split([" ", ":"], trim: true)
+    |> Enum.chunk_every(2)
+    |> Map.new(&List.to_tuple/1)
+  end
+
+  def is_valid?(passport) do
+    # passport |> IO.inspect
+    case map_size(passport) do
+      8 -> true
+      7 -> not Map.has_key?(passport, "cid")
+      _ -> false
+    end
+  end
+
+  def part1 do
+    input()
+    |> Enum.count(&is_valid?/1)
+  end
+
+  def part2 do
+  end
+end
