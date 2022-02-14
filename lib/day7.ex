da8d314798069e662634191d1d508cb2ce07cf4b
Michael Kaufman <michaelkauf@gmail.com>
2022-02-13 16:10:43 -0800

    add part1 version 2

--- lib/day7.ex
+++ lib/day7.ex
@@ -3,6 +3,9 @@ defmodule AdventOfCode.Day7 do
   Documentation for `AdventOfCode.Day7`.
   """
 
+  @sg "shiny gold"
+  @no "no other"
+
   def input do
     "priv/day7/input.txt"
     |> File.read!()
@@ -16,26 +19,41 @@ defmodule AdventOfCode.Day7 do
     |> Map.new(fn [bag | contents] -> {bag, contents} end)
   end
 
-  def contain_gold?("no other"), do: false
-  def contain_gold?("shiny gold"), do: true
+  def contain_gold?(@no), do: false
+  def contain_gold?(@sg), do: true
   def contain_gold?(<<_n::utf8, " ">> <> bags), do: contain_gold?(bags)
   def contain_gold?(bags) when is_binary(bags), do: contain_gold?(rules()[bags])
   def contain_gold?([head | []]), do: contain_gold?(head)
   def contain_gold?([head | tail]), do: contain_gold?(head) or contain_gold?(tail)
 
-  def sum_bags("shiny gold"), do: sum_bags("shiny gold", 0)
-  def sum_bags("no other", sum), do: sum
-  def sum_bags(<<n::utf8, " ">> <> bags, sum), do: sum_bags(bags, sum + n)
-  def sum_bags(bags, sum) when is_binary(bags), do: sum_bags(rules()[bags], sum)
-  def sum_bags([head | []], sum), do: sum_bags(head, sum)
-  def sum_bags([head | tail], sum), do: sum_bags(head, sum) or sum_bags(tail, sum)
+  def traverse(@no, acc), do: acc
+  def traverse(@sg, acc), do: List.insert_at(acc, 0, @sg)
+  def traverse(<<_n::utf8, " ">> <> bags, acc), do: traverse(bags, acc)
+  def traverse(bags, acc) when is_binary(bags) do
+    traverse(rules()[bags], List.insert_at(acc, 0, bags))
+  end
+  def traverse([head | []], acc) do 
+    traverse(head, acc)
+  end
+  def traverse([head | tail], acc) do
+    traverse(head, acc) ++ traverse(tail, acc)
+  end
+
+  def part1_version_two do
+    rules()
+    |> Map.delete(@sg)
+    |> Enum.map(fn {k, _v} -> traverse(k, []) end)
+    |> IO.inspect()
+    |> Enum.count(&Enum.any?(&1, fn x -> x == @sg end))
+  end
 
   def part1 do
     rules()
-    |> Enum.map(fn {k, _v} -> if k != "shiny gold", do: contain_gold?(k) end)
+    |> Enum.map(fn {k, _v} -> if k != @sg, do: contain_gold?(k) end)
     |> Enum.count(&(&1 == true))
   end
 
   def part2 do
   end
 end
+AdventOfCode.Day7.part1() |> IO.inspect
