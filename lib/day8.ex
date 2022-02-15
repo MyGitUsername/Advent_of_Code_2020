08c3f4e82eebe6a5d0f6e118a23111457b0f122c
Michael Kaufman <michaelkauf@gmail.com>
2022-02-15 08:51:39 -0800

    refactor: improve var names

--- lib/day8.ex
+++ lib/day8.ex
@@ -9,7 +9,7 @@ defmodule AdventOfCode.Day8 do
     |> String.split(["\n", " "], trim: true)
     |> Enum.chunk_every(2)
     |> Enum.map(&List.to_tuple/1)
-    |> Enum.map(fn {op, arg} -> {String.to_atom(op), String.to_integer(arg)} end)
+    |> Enum.map(fn {op, arg} -> {String.to_existing_atom(op), String.to_integer(arg)} end)
   end
 
   defp execute({:nop, _}), do: {1, 0}
@@ -17,20 +17,20 @@ defmodule AdventOfCode.Day8 do
   defp execute({:jmp, offset}), do: {offset, 0}
 
   defp run(list), do: run(list, 0, [], 0)
-  defp run(instructions, pos, _visited, acc) when pos == length(instructions), do: {:ok, acc}
+  defp run(prog, curr, _hist, acc) when curr == length(prog), do: {:ok, acc}
 
-  defp run(instructions, pos, visited, acc) do
-    instruct = Enum.at(instructions, pos)
+  defp run(prog, curr, hist, acc) do
+    instruct = Enum.at(prog, curr)
 
-    if Enum.member?(visited, pos) do
+    if Enum.member?(hist, curr) do
       {:error, acc}
     else
       {offset, add_to_acc} = execute(instruct)
-      run(instructions, pos + offset, [pos | visited], acc + add_to_acc)
+      run(prog, curr + offset, [curr | hist], acc + add_to_acc)
     end
   end
 
-  defp test_modified_instructions({{op, _arg}, idx}) do
+  defp test_modified_prog({{op, _arg}, idx}) do
     op = if op == :jmp, do: :nop, else: :jmp
     {_, value} = Enum.at(input(), idx)
     modified_list = List.replace_at(input(), idx, {op, value})
@@ -47,7 +47,7 @@ defmodule AdventOfCode.Day8 do
     input()
     |> Enum.with_index()
     |> Enum.filter(fn {{op, _arg}, _idx} -> op == :jmp or op == :nop end)
-    |> Enum.map(&test_modified_instructions(&1))
+    |> Enum.map(&test_modified_prog(&1))
     |> Enum.reject(&(&1 == :error))
   end
 end
